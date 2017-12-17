ffc11801367b0d815eaae489f1b80339c7329c9e
TanyaGir <tanyagir24@gmail.com>
2017-12-16 22:07:38 -0600

    Add files via upload

---
+++ mydatabase_setup.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+import datetime
+
+Base = declarative_base()
+
+class Category(Base):
+
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name =  Column(String(250), nullable=False)
+    item_list = relationship("Item", cascade="all, delete-orphan")
+
+    @property
+    def serialize(self):
+           """Return object data in easily serializeable format"""
+           return {
+               'name'         : self.name,
+               'id'           : self.id,
+           }
+
+class Item(Base):
+
+    __tablename__ = 'item'
+
+    item_name = Column(String(250), nullable=False)
+    item_id = Column(Integer, primary_key=True)
+    item_date = Column(DateTime, default=datetime.datetime.now)
+    item_description = Column(String(250), nullable=False)
+    cat_id = Column(Integer,ForeignKey('category.id'))
+    category = relationship (Category)
+
+    @property
+    def serialize(self):
+       """Return object data in easily serializeable format"""
+       return {
+           'item_name'         : self.name,
+           'id'                : self.id,
+           'date'              : self.date,
+           'description'       : self.description,
+       }
+
+engine = create_engine('sqlite:///catalog.db')
+
+Base.metadata.create_all(engine)
